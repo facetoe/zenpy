4b769ce79b19d5cb7f51fa2de123d9bdad1b06e5
facetoe <facetoe@ymail.com>
2015-02-08 22:42:22 +0800

    Added more detail to setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ __author__ = 'facetoe'
 from setuptools import setup
 
 setup(name='zenpy',
-      version='0.1',
+      version='0.1.0',
       install_requires=['requests'],
-      description='Wrapper for the zendesk API',
-      url='',
+      description='Wrapper for the Zendesk API',
+      url='https://github.com/facetoe/zenpy',
       author='facetoe',
       author_email='facetoe@ymail.com',
       license='MIT',
