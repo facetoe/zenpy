a94882f37d962ee4d609734704b1d6641ec7cece
facetoe <facetoe@ymail.com>
2015-08-30 21:47:09 +0800

    Beta release

--- setup.py
+++ setup.py
@@ -5,14 +5,13 @@ import setuptools
 setup(
 	name='zenpy',
 	packages=setuptools.find_packages(),
-	version='0.0.13',
+	version='0.0.14',
 	description='Python wrapper for the Zendesk API',
-	long_description=open('README.md').read(),
 	license='GPLv3',
 	author='Face Toe',
 	author_email='facetoe@facetoe.com.au',
 	url='https://github.com/facetoe/zenpy',
-	download_url='https://github.com/facetoe/zenpy/releases/tag/0.0.13',
+	download_url='https://github.com/facetoe/zenpy/releases/tag/0.0.14',
 	install_requires=[
 		'requests',
 		'python-dateutil',
