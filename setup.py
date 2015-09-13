834db8b3f38a9d0b9de268e3a518b5f9f51624a9
facetoe <facetoe@ymail.com>
2015-09-13 21:28:23 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ import setuptools
 setup(
 	name='zenpy',
 	packages=setuptools.find_packages(),
-	version='0.0.17',
+	version='0.0.18',
 	description='Python wrapper for the Zendesk API',
 	license='GPLv3',
 	author='Face Toe',
 	author_email='facetoe@facetoe.com.au',
 	url='https://github.com/facetoe/zenpy',
-	download_url='https://github.com/facetoe/zenpy/releases/tag/0.0.17',
+	download_url='https://github.com/facetoe/zenpy/releases/tag/0.0.18',
 	install_requires=[
 		'requests',
 		'python-dateutil',
