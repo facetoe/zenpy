4ed3554124eee5a7f425909aee1baa14b5ded5f4
facetoe <facetoe@ymail.com>
2015-09-20 12:38:28 +0800

    Fix photo so it works correctly

---
+++ zenpy/lib/objects/photo.py
@@ -0,0 +1,22 @@
+
+import dateutil.parser
+from zenpy.lib.objects.base_object import BaseObject
+
+class Photo(BaseObject):
+	def __init__(self, api=None):
+		self.api = api
+		self.content_url = None
+		self.name = None
+		self.id = None
+		self._thumbnails = None
+		self.content_type = None
+		self.size = None
+
+	@property
+	def thumbnails(self):
+		if self.api and self._thumbnails:
+			for thumbnail in self._thumbnails:
+				yield self.api.object_manager.object_from_json('thumbnail', thumbnail)
+
+
+
