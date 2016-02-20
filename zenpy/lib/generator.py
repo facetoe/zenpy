0bb3080f3daaa2ec68c0d5bf1c414050e9c7cdd0
facetoe <facetoe@facetoe.com.au>
2016-02-20 22:08:52 +0800

    Simplify generator.

--- zenpy/lib/generator.py
+++ zenpy/lib/generator.py
@@ -54,30 +54,30 @@ class ResultGenerator(object):
         return self.next()
 
     def next(self):
+        # Update end_time etc
+        self.update_attrs(self._json)
+
         # Pagination
         if self.position >= len(self.values):
+            end_time = self._json.get('end_time', None)
+            next_page = self._json.get('next_page', None)
+
             # If we are calling an incremental API, make sure to honour the restrictions
-            if 'end_time' in self._json and self._json['end_time']:
-
-                # I'm not sure if this is being handled correctly. If we simply continue iterating
-                # while there are still items we end up in an infinite loop that returns the same item
-                # over and over again.
-                # If we stop iteration when the end_time is equal to the previous end_time we occasionally
-                # stop prematurely when a very high number of tickets was created for that instant.
-                if self.end_time == self._json['end_time'] and self._json['count'] <= 1:
+            if end_time:
+                # Incremental APIs return values in blocks of 1000 and the count value is
+                # decremented by 1000 for each block that is consumed. If we have < 1000
+                # then there is nothing left to retrieve.
+                if self._json.get('count', 0) < 1000:
                     raise StopIteration
-
-                # Update end_time etc
-                self.update_attrs(self._json)
 
                 # We can't request updates from an incremental api if the
                 # start_time value is less than 5 minutes in the future.
                 # (end_time is added as start_time to the next_page URL)
-                if (datetime.fromtimestamp(int(self._json['end_time'])) + timedelta(minutes=5)) > datetime.now():
+                if (datetime.fromtimestamp(int(end_time)) + timedelta(minutes=5)) > datetime.now():
                     raise StopIteration
 
-            if self._json.get('next_page'):
-                self._json = self._get_as_json(self._json.get('next_page'))
+            if next_page:
+                self._json = self._get_as_json(next_page)
                 self.values = self._json[self.result_key]
                 self.position = 0
             else:
