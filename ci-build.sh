3f6dc69f09182cb91dc5dfdb405e125392ec180c
Facetoe <facetoe@facetoe.com.au>
2017-04-17 22:14:30 +0700

    Add scripts for Travis CI.

---
+++ ci-build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -xe
+
+python3 --version
+py.test --version
+python3 -mpycodestyle --version
+
+# Target directory for all build files
+BUILD=${1:-ci-build}
+rm -rf ${BUILD}/
+mkdir -p $BUILD
+
+python3 -mpycodestyle --ignore E501,E741,E305,E722,E402 tests zenpy
+
+# Check that the setup.py script works
+rm -rf ${BUILD}/test-install ${BUILD}/test-install-bin
+mkdir ${BUILD}/test-install ${BUILD}/test-install-bin
+PYTHONPATH=${BUILD}/test-install python3 setup.py --quiet install --install-lib ${BUILD}/test-install --install-scripts ${BUILD}/test-install-bin
+
+test -f ${BUILD}/test-install-bin/zenpy
+
+PYTHONPATH=${BUILD}/test-install python3 -m unittest discover -t zenpy/tests
+
