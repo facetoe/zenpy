b46d54eae5f5d521876f0ed43f82caa7a74b31cc
Facetoe <facetoe@facetoe.com.au>
2017-05-03 14:32:31 +0700

    Don't write BaseObject twice.

--- tools/gen_classes.py
+++ tools/gen_classes.py
@@ -334,4 +334,4 @@ def process_specification_directory(glob_pattern, outfile_name, write_baseclass=
 
 
 process_specification_directory('zendesk/*.json', 'api_objects/__init__.py')
-process_specification_directory('chat/*.json', 'api_objects/chat_objects.py')
+process_specification_directory('chat/*.json', 'api_objects/chat_objects.py', write_baseclass=False)
