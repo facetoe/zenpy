060c8b2b7b3b3b4a3bb6d50f34621ba2bf139121
Facetoe <facetoe@facetoe.com.au>
2017-05-19 14:44:51 +0700

    Add test for SearchResultGenerator.
    Prevent regression reported in #130, closes #131.

---
+++ tests/test_api/test_generator.py
@@ -0,0 +1,15 @@
+from test_api.fixtures import ZenpyApiTestCase
+from zenpy.lib.generator import SearchResultGenerator
+
+
+class TestSearchGenerator(ZenpyApiTestCase):
+    __test__ = True
+
+    def test_search_generator_len(self):
+        with self.recorder.use_cassette(self.generate_cassette_name(), serialize_with='prettyjson'):
+            search_generator = self.zenpy_client.search(type='ticket')
+            self.assertEqual(len(search_generator), len([t for t in search_generator]))
+
+    def test_search_returns_search_result_generator(self):
+        search_generator = self.zenpy_client.search(type='ticket')
+        self.assertIsInstance(search_generator, SearchResultGenerator)
