5b5a50e83ff1cc2cff9d1753ce6cda245cfaacc5
Facetoe <facetoe@facetoe.com.au>
2017-12-21 20:22:37 +1100

    Set object attributes properly.

--- tools/gen_classes.py
+++ tools/gen_classes.py
@@ -260,7 +260,7 @@ class BaseObject(object):
 
     def __setattr__(self, key, value):
         self.__dict__['_dirty_attributes'].add(key)
-        self.__dict__[key] = value
+        object.__setattr__(self, key, value)
 
     def _clean_dirty(self):
         self.__dict__['_dirty_attributes'].clear()
