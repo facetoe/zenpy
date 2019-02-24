fdae24b9aa45000afec1f3368e4bc39db05837be
Andrey Shpak <ashpak@ashpak.ru>
2019-02-24 21:36:28 +0300

    Fixes #75 Moving version information to one place

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup
 import setuptools
+from zenpy import __version__
 
 setup(
     name='zenpy',
     packages=setuptools.find_packages(),
-    version='2.0.8',
+    version=__version__,
     description='Python wrapper for the Zendesk API',
     license='GPLv3',
     author='Face Toe',
     author_email='facetoe@facetoe.com.au',
     url='https://github.com/facetoe/zenpy',
-    download_url='https://github.com/facetoe/zenpy/releases/tag/2.0.8',
+    download_url='https://github.com/facetoe/zenpy/releases/tag/{}'.format(__version__),
     install_requires=[
         'requests>=2.7.0',
         'python-dateutil>=2.4.0',
