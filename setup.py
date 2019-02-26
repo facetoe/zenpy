13d614dc7f0a0bc857eaddc2697fa6d8c37fd56e
Andrey Shpak <ashpak@ashpak.ru>
2019-02-26 19:03:40 +0300

    requirments and setup.py dependencies sync and version update

--- setup.py
+++ setup.py
@@ -13,18 +13,25 @@ setup(
     url='https://github.com/facetoe/zenpy',
     download_url='https://github.com/facetoe/zenpy/releases/tag/{}'.format(__version__),
     install_requires=[
-        'requests>=2.7.0',
-        'python-dateutil>=2.4.0',
-        'cachetools>=1.0.3',
-        'pytz>=2017.3',
-        'future>=0.16.0'
+        'requests>=2.14.2',
+        'python-dateutil>=2.7.5',
+        'cachetools>=3.1.0',
+        'pytz>=2018.9',
+        'future>=0.17.1'
     ],
     keywords=['zendesk', 'api', 'wrapper'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
     ],
 )
