9a77b323b48124ab210555a1941c0e3e076dae6b
Facetoe <facetoe@facetoe.com.au>
2021-01-30 15:36:57 +1100

    Bump version

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ import setuptools
 setup(
     name='zenpy',
     packages=setuptools.find_packages(),
-    version='2.0.22',
+    version='2.0.23',
     description='Python wrapper for the Zendesk API',
     license='GPLv3',
     author='Face Toe',
     author_email='facetoe@facetoe.com.au',
     url='https://github.com/facetoe/zenpy',
-    download_url='https://github.com/facetoe/zenpy/releases/tag/2.0.22',
+    download_url='https://github.com/facetoe/zenpy/releases/tag/2.0.23',
     install_requires=[
         'requests>=2.14.2',
         'python-dateutil>=2.7.5',
