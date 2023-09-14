105cb4e31c90015763de8b87acb8e65e89259316
Joshua Teitelbaum <joshuat@gmail.com>
2023-09-14 00:04:04 -0700

    bump (#581)

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ import setuptools
 setup(
     name='zenpy',
     packages=setuptools.find_packages(),
-    version='2.0.37',
+    version='2.0.38',
     description='Python wrapper for the Zendesk API',
     license='GPLv3',
     license_files = ('LICENSE'),
     author='Face Toe',
     author_email='facetoe@facetoe.com.au',
     url='https://github.com/facetoe/zenpy',
-    download_url='https://github.com/facetoe/zenpy/releases/tag/2.0.37',
+    download_url='https://github.com/facetoe/zenpy/releases/tag/2.0.38',
     install_requires=[
         'requests>=2.14.2',
         'python-dateutil>=2.7.5',
--- zenpy/__init__.py
+++ zenpy/__init__.py
@@ -58,7 +58,7 @@ if debug_log is not None:
 log = logging.getLogger()
 
 __author__ = "facetoe"
-__version__ = "2.0.37"
+__version__ = "2.0.38"
 
 
 class Zenpy(object):
